e94b2489c3bdb45fd88d29d0884279674a55d4db
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2025-12-20 16:50:08 -0600

    started event loop

--- main.cpp
+++ main.cpp
@@ -12,6 +12,30 @@ struct Pipe {
 
 int main() {
 
+    Bird bird;
+    bird.x = 0;
+    bird.y = 50;
+    bird.vx = 1;
+    bird.vy = -1;
+    Pipe pipe;
+    
+
+    int score = 0;
+
+    for (int t = 0; t < 1000; t++ ) {
+
+        // update bird position
+        bird.y -= bird.vy;
+        bird.x += bird.vx;
+
+        // check bird bounds
+        if (bird.y < 0 || bird.y > 100) {
+            std::cout << "Bird out of bounds at tick " << t << "\n";
+            break;
+        };
+            
+    }
+
     return 0;
     
 }
